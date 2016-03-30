a92aec0717c5408f031a1bce9b343682031851a6
Cody Johnson <Cody@johnsonwebdev.com>
2016-03-29 23:47:07 -0500

    Cleaned Up Form View

--- Room_Schedule/Form2.Designer.cs
+++ Room_Schedule/Form2.Designer.cs
@@ -208,7 +208,8 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form2";
-            this.Text = "Form2";
+            this.Text = "Schedule Room";
+            this.Load += new System.EventHandler(this.Scheduling_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
--- Room_Schedule/Form2.cs
+++ Room_Schedule/Form2.cs
@@ -45,5 +45,10 @@ namespace Room_Schedule
         {
 
         }
+
+        private void Scheduling_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
