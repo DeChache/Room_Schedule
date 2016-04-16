cae860bb4cb1a5613e9d15e4a04c4d57b316d66f
Cody Johnson <Cody@johnsonwebdev.com>
2016-04-16 00:15:58 -0500

    Finished for the day

--- Room_Schedule/Form2.cs
+++ Room_Schedule/Form2.cs
@@ -13,12 +13,14 @@ namespace Room_Schedule
 {
     public partial class Form2 : Form
     {
+        SQLiteConnection m_dbConnection;
+        m_dbConnection = new SQLiteConnection("Data Source=room_schedule.db;Version=3;");
+        m_dbConnection.Open();
+
         public Form2()
         {
             InitializeComponent();
-            SQLiteConnection m_dbConnection;
-            m_dbConnection = new SQLiteConnection("Data Source=room_schedule.db;Version=3;");
-            m_dbConnection.Open();
+           
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -28,16 +30,23 @@ namespace Room_Schedule
 
         private void button1_Click(object sender, EventArgs e)
         {
+            var dataInsert = new SQLiteCommand(m_dbConnection);
             var checkedButton = groupBox1.Controls.OfType<RadioButton>()
                                       .FirstOrDefault(r => r.Checked);
+            var scheduleDate = dateTimePicker1.Value.Month + "/" + dateTimePicker1.Value.Day + "/" + dateTimePicker1.Value.Year;
+            var scheduleTime = dateTimePicker2.Value.Hour + ":" + dateTimePicker2.Value.Minute;
 
-            MessageBox.Show("The selected date is " + dateTimePicker1.Value.Month + "/" + dateTimePicker1.Value.Day + "/" 
-            + dateTimePicker1.Value.Year +
-            " The selcted time is " + dateTimePicker2.Value.Hour + ":" + dateTimePicker2.Value.Minute +
-            " The selected room is " + checkedButton.Name +
-            " The Value of Box 1 is " + this.textBox1.Text +
-            " The Value of Box 2 is " + this.textBox2.Text +
-            " The Value of Box 3 is " + this.textBox3.Text);
+            dataInsert.CommandText =
+                "INSERT INTO Room_Schedule (ScheduleDate,Room,Description1,Description2,Description3) VALUES (scheduleDate, 'Doe');";
+            dataInsert.ExecuteNonQuery();
+
+            //            MessageBox.Show("The selected date is " + dateTimePicker1.Value.Month + "/" + dateTimePicker1.Value.Day + "/" 
+            //            + dateTimePicker1.Value.Year +
+            //            " The selcted time is " + dateTimePicker2.Value.Hour + ":" + dateTimePicker2.Value.Minute +
+            //            " The selected room is " + checkedButton.Name +
+            //            " The Value of Box 1 is " + this.textBox1.Text +
+            //            " The Value of Box 2 is " + this.textBox2.Text +
+            //            " The Value of Box 3 is " + this.textBox3.Text);
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
