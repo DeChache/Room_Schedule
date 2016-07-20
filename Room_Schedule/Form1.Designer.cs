395acb91b12fc97dcd3693210fe46266f7c78708
Cody Johnson <cody@johnsonwebdev.com>
2016-07-20 01:31:59 -0500

    Printing works

--- Room_Schedule/Form1.Designer.cs
+++ Room_Schedule/Form1.Designer.cs
@@ -37,11 +37,11 @@
             this.button3 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.printDialog1 = new System.Windows.Forms.PrintDialog();
             this.printDocument1 = new System.Drawing.Printing.PrintDocument();
-            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -73,7 +73,6 @@
             // 
             // button1
             // 
-            this.button1.AutoEllipsis = true;
             this.button1.AutoSize = true;
             this.button1.Location = new System.Drawing.Point(13, 76);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -121,7 +120,6 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(800, 600);
             this.dataGridView1.TabIndex = 4;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // dateTimePicker1
             // 
@@ -132,23 +130,21 @@
             this.dateTimePicker1.Size = new System.Drawing.Size(160, 26);
             this.dateTimePicker1.TabIndex = 5;
             // 
-            // printDialog1
-            // 
-            this.printDialog1.UseEXDialog = true;
-            // 
             // printDocument1
             // 
             this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
             // 
-            // printPreviewDialog1
+            // dataGridView2
             // 
-            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
-            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
-            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
-            this.printPreviewDialog1.Enabled = true;
-            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
-            this.printPreviewDialog1.Name = "printPreviewDialog1";
-            this.printPreviewDialog1.Visible = false;
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(0, 0);
+            this.dataGridView2.TabIndex = 6;
             // 
             // Form1
             // 
@@ -156,6 +152,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(985, 381);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
@@ -171,6 +168,7 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,9 +184,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.PrintDialog printDialog1;
         private System.Drawing.Printing.PrintDocument printDocument1;
-        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 
--- Room_Schedule/Form1.cs
+++ Room_Schedule/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,7 +18,8 @@ namespace Room_Schedule
     public partial class Form1 : Form
     {
         DataTable schedule_nice = new DataTable();
-        
+        DataTable schedule_print = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
@@ -173,18 +175,62 @@ namespace Room_Schedule
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        public void initializePrintTable()
         {
-            printDocument1.Print();
+            schedule_print.Columns.Add("Description3");
+            schedule_print.Columns.Add("Description2");
+            schedule_print.Columns.Add("Description1");
+            schedule_print.Columns.Add("Room");
+            schedule_print.Columns.Add("Time");
+            schedule_print.Columns.Add("Date");
+                       
         }
 
-     
-
-        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        public void printDataDisplay(int Date)
         {
-            Bitmap bm = new Bitmap(this.dataGridView1.Width, this.dataGridView1.Height);
-            dataGridView1.DrawToBitmap(bm, new Rectangle(0, 0, this.dataGridView1.Width, this.dataGridView1.Height));
-            e.Graphics.DrawImage(bm, 0, 0);
+            //Clear Data table for new data
+
+            schedule_print.Clear();
+
+            //Data table for raw data from database
+            DataTable schedule = new DataTable();
+            SQLiteConnection m_dbConnection;
+            m_dbConnection = new SQLiteConnection("Data Source=room_schedule.db;Version=3;");
+            m_dbConnection.Open();
+            var dataRetrieve = new SQLiteCommand(m_dbConnection);
+            dataRetrieve.CommandText = "Select * from Room_Schedule where ScheduleDate=" + Date + ";";
+            SQLiteDataAdapter scheduleData = new SQLiteDataAdapter(dataRetrieve);
+            scheduleData.Fill(schedule);
+            foreach (DataRow data in schedule.Rows)
+            {
+                //Convert date from Unix time to human readable time
+                int unix_date = Convert.ToInt32(data["ScheduleDate"]);
+                DateTimeOffset standard_date = DateTimeOffset.FromUnixTimeSeconds(unix_date);
+
+                //Convert time from Unix time to human readable time
+                //int unix_time = Convert.ToInt32(data["ScheduleTime"]);
+                //DateTimeOffset standard_time = DateTimeOffset.FromUnixTimeSeconds(unix_time);
+                //DateTime standard_12_hour = standard_time; 
+
+                //Convert datetime to string
+                //DateTime Local_Time = standard_time.DateTime;
+                String display_date = standard_date.Month + "/" + standard_date.Day + "/" + standard_date.Year;
+                //String display_time = Local_Time.ToString("hh" + ":" + "mm" + "tt");
+
+                //Add data to nice data table
+                DataRow Print_data = schedule_print.NewRow();
+                Print_data["Date"] = display_date;
+                Print_data["Time"] = data["ScheduleTime"];
+                Print_data["Room"] = data["Room"];
+                Print_data["Description1"] = data["Description1"];
+                Print_data["Description2"] = data["Description2"];
+                Print_data["Description3"] = data["Description3"];
+                schedule_print.Rows.Add(Print_data);
+                //MessageBox.Show("The row data is " + data) ;
+
+            }
+        
+            dataGridView2.DataSource = schedule_print;
         }
 
         public void CheckDB()
@@ -204,7 +250,7 @@ namespace Room_Schedule
                 m_dbConnection = new SQLiteConnection("Data Source=room_schedule.db;Version=3;");
                 m_dbConnection.Open();
 
-                string createTables = "CREATE TABLE \"Room_Schedule\" ('Index'INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,'ScheduleDate'INTEGER NOT NULL,'ScheduleTime'	TEXT,'Room'	TEXT,'Description1'	TEXT,'Description2'	TEXT,'Description3'	TEXT)" ;
+                string createTables = "CREATE TABLE \"Room_Schedule\" ('Index'INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,'ScheduleDate'INTEGER NOT NULL,'ScheduleTime'	TEXT,'Room'	TEXT,'Description1'	TEXT,'Description2'	TEXT,'Description3'	TEXT)";
 
                 SQLiteCommand command = new SQLiteCommand(createTables, m_dbConnection);
                 command.ExecuteNonQuery();
@@ -228,12 +274,247 @@ namespace Room_Schedule
             return Today_UNIX;
         }
 
- 
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
+            //printDocument1.Print();
+            initializePrintTable();
+            printDataDisplay(getUnixTime());
+            ClsPrint _ClsPrint = new ClsPrint(dataGridView2, "Schedule");
+            _ClsPrint.PrintForm();
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            initializePrintTable();           
+            printDataDisplay(getUnixTime());
+            ClsPrint _ClsPrint = new ClsPrint(dataGridView2, "Schedule");
+            _ClsPrint.PrintForm();
+        }
+
+        // From http://stackoverflow.com/questions/15853746/how-to-print-values-from-a-datagridview-control 
+        class ClsPrint
+        {
+            #region Variables
+
+            int iCellHeight = 0; //Used to get/set the datagridview cell height
+            int iTotalWidth = 0; //
+            int iRow = 0;//Used as counter
+            bool bFirstPage = false; //Used to check whether we are printing first page
+            bool bNewPage = false;// Used to check whether we are printing a new page
+            int iHeaderHeight = 0; //Used for the header height
+            StringFormat strFormat; //Used to format the grid rows.
+            ArrayList arrColumnLefts = new ArrayList();//Used to save left coordinates of columns
+            ArrayList arrColumnWidths = new ArrayList();//Used to save column widths
+            private PrintDocument _printDocument = new PrintDocument();
+            private DataGridView gw = new DataGridView();
+            private string _ReportHeader;
+
+            #endregion
+
+            public ClsPrint(DataGridView gridview, string ReportHeader)
+            {
+                _printDocument.PrintPage += new PrintPageEventHandler(_printDocument_PrintPage);
+                _printDocument.BeginPrint += new PrintEventHandler(_printDocument_BeginPrint);
+                gw = gridview;
+                _ReportHeader = ReportHeader;
+            }
+
+            public void PrintForm()
+            {
+                ////Open the print dialog
+                //PrintDialog printDialog = new PrintDialog();
+                //printDialog.Document = _printDocument;
+                //printDialog.UseEXDialog = true;
+
+                ////Get the document
+                //if (DialogResult.OK == printDialog.ShowDialog())
+                //{
+                //    _printDocument.DocumentName = "Test Page Print";
+                //    _printDocument.Print();
+                //}
+
+                //Open the print preview dialog
+                PrintPreviewDialog objPPdialog = new PrintPreviewDialog();
+                objPPdialog.Document = _printDocument;
+                objPPdialog.ShowDialog();
+            }
+
+            private void _printDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+            {
+                //try
+                //{
+                //Set the left margin
+                int iLeftMargin = e.MarginBounds.Left;
+                //Set the top margin
+                int iTopMargin = e.MarginBounds.Top;
+                //Whether more pages have to print or not
+                bool bMorePagesToPrint = false;
+                int iTmpWidth = 0;
+
+                //For the first page to print set the cell width and header height
+                if (bFirstPage)
+                {
+                    foreach (DataGridViewColumn GridCol in gw.Columns)
+                    {
+                        iTmpWidth = (int)(Math.Floor((double)((double)GridCol.Width /
+                            (double)iTotalWidth * (double)iTotalWidth *
+                            ((double)e.MarginBounds.Width / (double)iTotalWidth))));
+
+                        iHeaderHeight = (int)(e.Graphics.MeasureString(GridCol.HeaderText,
+                            GridCol.InheritedStyle.Font, iTmpWidth).Height) + 11;
+
+                        // Save width and height of headers
+                        arrColumnLefts.Add(iLeftMargin);
+                        arrColumnWidths.Add(iTmpWidth);
+                        iLeftMargin += iTmpWidth;
+                    }
+                }
+                //Loop till all the grid rows not get printed
+                while (iRow <= gw.Rows.Count - 1)
+                {
+                    DataGridViewRow GridRow = gw.Rows[iRow];
+                    //Set the cell height
+                    iCellHeight = GridRow.Height + 5;
+                    int iCount = 0;
+                    //Check whether the current page settings allows more rows to print
+                    if (iTopMargin + iCellHeight >= e.MarginBounds.Height + e.MarginBounds.Top)
+                    {
+                        bNewPage = true;
+                        bFirstPage = false;
+                        bMorePagesToPrint = true;
+                        break;
+                    }
+                    else
+                    {
+
+                        if (bNewPage)
+                        {
+                            //Draw Header
+                            e.Graphics.DrawString(_ReportHeader,
+                                new Font(gw.Font, FontStyle.Bold),
+                                Brushes.Black, e.MarginBounds.Left,
+                                e.MarginBounds.Top - e.Graphics.MeasureString(_ReportHeader,
+                                new Font(gw.Font, FontStyle.Bold),
+                                e.MarginBounds.Width).Height - 13);
+
+                            String strDate = "";
+                            //Draw Date
+                            e.Graphics.DrawString(strDate,
+                                new Font(gw.Font, FontStyle.Bold), Brushes.Black,
+                                e.MarginBounds.Left +
+                                (e.MarginBounds.Width - e.Graphics.MeasureString(strDate,
+                                new Font(gw.Font, FontStyle.Bold),
+                                e.MarginBounds.Width).Width),
+                                e.MarginBounds.Top - e.Graphics.MeasureString(_ReportHeader,
+                                new Font(new Font(gw.Font, FontStyle.Bold),
+                                FontStyle.Bold), e.MarginBounds.Width).Height - 13);
+
+                            //Draw Columns                 
+                            iTopMargin = e.MarginBounds.Top;
+                            DataGridViewColumn[] _GridCol = new DataGridViewColumn[gw.Columns.Count];
+                            int colcount = 0;
+                            //Convert ltr to rtl
+                            foreach (DataGridViewColumn GridCol in gw.Columns)
+                            {
+                                _GridCol[colcount++] = GridCol;
+                            }
+                            for (int i = (_GridCol.Count() - 1); i >= 0; i--)
+                            {
+                                e.Graphics.FillRectangle(new SolidBrush(Color.LightGray),
+                                    new Rectangle((int)arrColumnLefts[iCount], iTopMargin,
+                                    (int)arrColumnWidths[iCount], iHeaderHeight));
+
+                                e.Graphics.DrawRectangle(Pens.Black,
+                                    new Rectangle((int)arrColumnLefts[iCount], iTopMargin,
+                                    (int)arrColumnWidths[iCount], iHeaderHeight));
+
+                                e.Graphics.DrawString(_GridCol[i].HeaderText,
+                                    _GridCol[i].InheritedStyle.Font,
+                                    new SolidBrush(_GridCol[i].InheritedStyle.ForeColor),
+                                    new RectangleF((int)arrColumnLefts[iCount], iTopMargin,
+                                    (int)arrColumnWidths[iCount], iHeaderHeight), strFormat);
+                                iCount++;
+                            }
+                            bNewPage = false;
+                            iTopMargin += iHeaderHeight;
+                            
+                        }
+                        iCount = 0;
+                        DataGridViewCell[] _GridCell = new DataGridViewCell[GridRow.Cells.Count];
+                        int cellcount = 0;
+                        //Convert ltr to rtl
+                        foreach (DataGridViewCell Cel in GridRow.Cells)
+                        {
+                            _GridCell[cellcount++] = Cel;
+                        }
+                        //Draw Columns Contents                
+                        for (int i = (_GridCell.Count() - 1); i >= 0; i--)
+                        {
+                            if (_GridCell[i].Value != null)
+                            {
+                                e.Graphics.DrawString(_GridCell[i].FormattedValue.ToString(),
+                                    _GridCell[i].InheritedStyle.Font,
+                                    new SolidBrush(_GridCell[i].InheritedStyle.ForeColor),
+                                    new RectangleF((int)arrColumnLefts[iCount],
+                                    (float)iTopMargin,
+                                    (int)arrColumnWidths[iCount], (float)iCellHeight),
+                                    strFormat);
+                            }
+                            //Drawing Cells Borders 
+                            e.Graphics.DrawRectangle(Pens.Black,
+                                new Rectangle((int)arrColumnLefts[iCount], iTopMargin,
+                                (int)arrColumnWidths[iCount], iCellHeight));
+                            iCount++;
+                        }
+                    }
+                    iRow++;
+                    iTopMargin += iCellHeight;
+                }
+                //If more lines exist, print another page.
+                if (bMorePagesToPrint)
+                    e.HasMorePages = true;
+                else
+                    e.HasMorePages = false;
+                //}
+                //catch (Exception exc)
+                //{
+                //    MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK,
+                //       MessageBoxIcon.Error);
+                //}
+            }
+
+            private void _printDocument_BeginPrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+            {
+                try
+                {
+                    strFormat = new StringFormat();
+                    strFormat.Alignment = StringAlignment.Center;
+                    strFormat.LineAlignment = StringAlignment.Center;
+                    strFormat.Trimming = StringTrimming.EllipsisCharacter;
+
+                    arrColumnLefts.Clear();
+                    arrColumnWidths.Clear();
+                    iCellHeight = 0;
+                    iRow = 0;
+                    bFirstPage = true;
+                    bNewPage = true;
+
+                    // Calculating Total Widths
+                    iTotalWidth = 0;
+                    foreach (DataGridViewColumn dgvGridCol in gw.Columns)
+                    {
+                        iTotalWidth += dgvGridCol.Width;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
 
         }
+
+
     }
 
 
